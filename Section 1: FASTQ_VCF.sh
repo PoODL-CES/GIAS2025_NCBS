4cabf3aa15f11a471e401c11937b41e4c135641d
Haradhan2000 <rharadhan@iisc.ac.in>
2025-04-22 19:02:53 +0530

    Update Section 1: FASTQ_VCF.sh

--- Section 1: FASTQ_VCF.sh
+++ Section 1: FASTQ_VCF.sh
@@ -1,4 +1,5 @@
 ### Download the files BEN_CI16_sub_1.fq.gz BEN_CI16_sub_2.fq.gz BEN_NW13_sub_1.fq.gz BEN_NW13_sub_2.fq.gz
+
 ### conda environment-https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html
 
 ## Activate the conda environment
