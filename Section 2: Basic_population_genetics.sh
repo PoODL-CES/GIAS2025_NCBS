be2cbb9db6da55460467bb877485fab43f90e663
Pranav-Datar <pranavdatar01@gmail.com>
2025-04-22 19:36:01 +0530

    Update Section 2: Basic_population_genetics.sh

--- Section 2: Basic_population_genetics.sh
+++ Section 2: Basic_population_genetics.sh
@@ -1,1 +1,49 @@
+#PCA
 
+#firstly, .bed, .bim and .fam files need to be generated. from a vcf file. 
+#.bed: binary file that stores genotype calls for every individual at very SNP.
+#.bim: tab-delimited text file with information about each SNP.
+#.fam: tab-delimited text file; contains information about each individual/sample.
+
+ conda activate plink
+ plink --bfile output_file \
+  --pca 10 \
+  --allow-extra-chr \
+  --out output_file_pca
+
+#--bfile output_file: loads the plink binary dataset previously created using --make-bed
+#--pca 10: calculates the top 10 principal components
+#--allow-extra-chr: allows non-standard chromosome names
+#out output_file_pca: sets the prefix for output files (example: output_file_pca.eigenval)
+#eigenvalues and eigenvectors would be created
+
+conda activate r_env
+R
+install.packages("ggplot2")
+library(ggplot2)      
+eigenvec_data <- read.table("output_file_pca.eigenvec", header=FALSE)
+colnames(eigenvec_data) <- c("FID", "IID", paste("PC", 1:10, sep=""))
+head(eigenvec_data)
+ggplot(eigenvec_data, aes(x=PC1, y=PC2)) +
+  geom_point() +
+  labs(x="Principal Component 1", y="Principal Component 2", title="PCA Plot: PC1 vs PC2") +
+  theme_minimal()
+ggsave("pca_plot.png")
+q()
+
+scp username@IP_address:~/"path to the file on the remote cluster"/Rplots.pdf .
+scp username@IP_address:~/"path to the file on the remote cluster"/pca_plot.png .
+
+#R: Launches R
+#install.packages("ggplot2") library(ggplot2): installs and loads ggplot2 for plotting
+#eigenvec_data <- read.table("output_file_pca.eigenvec", header=FALSE): reads the .eigenvec file into the dataframe
+#colnames(eigenvec_data) <- c("FID", "IID", paste("PC", 1:10, sep="")): assigns column names; first 2 columns are FID and IID while next 10 columns are PC1 to PC10
+#head(eigenvec_data): displays the first few rows for the purpose of confirmation
+#ggplot(eigenvec_data, aes(x=PC1, y=PC2)) + geom_point() + labs(x="Principal Component 1", y="Principal Component 2", title="PCA Plot: PC1 vs PC2") + theme_minimal()
+       # Actually builds the graph
+#ggsave("pca_plot.png"): saves the plot in .png format
+#q(): Exits the R console
+
+#scp: secure copy protocol; copies files betweeen a local and a remote computer.
+# .: destination on the local machine. copies to the current directory.
+#pca_plot.png and Rplots.pdf would be saved in your local home directory
