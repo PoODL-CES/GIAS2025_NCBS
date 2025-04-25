c1be817fe4106b9292700080e897c9180aa44e20
anubhabkhan <anubhabkhan@gmail.com>
2025-04-25 17:02:48 +0530

    Update Section 2: Basic_population_genetics.sh

--- Section 2: Basic_population_genetics.sh
+++ Section 2: Basic_population_genetics.sh
@@ -11,7 +11,7 @@ Password:gias@ncbs
 
 conda activate plink
 
-plink --bfile /home/gias3/admin/input_files/machali_Aligned_rangeWideMerge_strelka_update2_BENGAL_mac3_passOnly_biallelicOnly_noIndels_minMAF0Pt05_chr_E2_minDP3_minQ30_minGQ30_hwe_0.05_noIndels_missing_mm0.6_meandepth95percentile_noZSB \
+plink --bfile input_files/machali_Aligned_rangeWideMerge_strelka_update2_BENGAL_mac3_passOnly_biallelicOnly_noIndels_minMAF0Pt05_chr_E2_minDP3_minQ30_minGQ30_hwe_0.05_noIndels_missing_mm0.6_meandepth95percentile_noZSB \
   --pca 5 \
   --allow-extra-chr \
   --out machali_Aligned_rangeWideMerge_strelka_update2_BENGAL_mac3_passOnly_biallelicOnly_noIndels_minMAF0Pt05_chr_E2_minDP3_minQ30_minGQ30_hwe_0.05_noIndels_missing_mm0.6_meandepth95percentile_noZSB
